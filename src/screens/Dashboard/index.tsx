26fdc50a2b8a55fe94df7e45be0c009801c45791
Gorgeous BSC token <84620683+higorgeous@users.noreply.github.com>
2021-08-22 00:23:38 +0930

    Fix dependancies

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,14 +1,11 @@
 import Hero from '@components/Hero';
-import { Web3Provider } from '@context/Web3Connect';
 
 import { Wrapper } from './styles';
 
 const DashboardScreen = () => (
-  <Web3Provider>
-    <Wrapper>
-      <Hero>Dashboard</Hero>
-    </Wrapper>
-  </Web3Provider>
+  <Wrapper>
+    <Hero>Dashboard</Hero>
+  </Wrapper>
 );
 
 export default DashboardScreen;
