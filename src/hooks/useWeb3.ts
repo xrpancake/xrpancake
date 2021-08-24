e216fc87dc117bfa7a83b722d135223f1b331ad7
Gorgeous BSC token <84620683+higorgeous@users.noreply.github.com>
2021-08-24 23:40:52 +0930

    Remove unused components

--- src/hooks/useWeb3.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import { Web3Context } from "@context/Web3Connect";
-
-const useWeb3 = () => {
-    return useContext(Web3Context);
-};
-
-export default useWeb3;
